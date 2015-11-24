9f331d18867911ef831120ac0afdcd8ea5c962aa
Joe Ryan <joe@clanryan.us>
2015-11-23 21:25:14 -0500

    complete xmlextract.py

--- xml-extract/xmlextract.py
+++ xml-extract/xmlextract.py
@@ -1,7 +1,21 @@
 # simple script to take a url from stdin, read XML using urllib, parse using
-# ElementTree, and sum up the numbers in comments
+# ElementTree, and sum up the numbers in count tags
 # Joe Ryan, 11/23/2015
 # completed as part of UofM class "Using Python to access Web Data" on coursera
 
 import urllib
 import xml.etree.ElementTree as ET
+
+dataurl = raw_input("Enter URL of XML to parse for sum of values: ")
+
+urlhandle = urllib.urlopen(dataurl)
+xmldata = urlhandle.read()
+
+tree = ET.fromstring(xmldata)
+results = tree.findall('.//count')
+
+summation = 0
+for result in results:
+    summation += int(result.text)
+
+print(summation)
