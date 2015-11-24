a6db7fe54e9504ec55ad4da53014266bb644607b
Joe Ryan <joe@clanryan.us>
2015-11-23 21:07:13 -0500

    Started xmlextract.py, for week 5

---
+++ xml-extract/xmlextract.py
@@ -0,0 +1,7 @@
+# simple script to take a url from stdin, read XML using urllib, parse using
+# ElementTree, and sum up the numbers in comments
+# Joe Ryan, 11/23/2015
+# completed as part of UofM class "Using Python to access Web Data" on coursera
+
+import urllib
+import xml.etree.ElementTree as ET
